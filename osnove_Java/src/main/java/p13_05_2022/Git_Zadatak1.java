8d378ea731f29e3dd2bcda420b02d5abd5844140
Sandra Milic <sandra.psihosfera@gmail.com>
2022-05-13 20:37:03 +0200

    Predavanja 13.05 Git Zadatak 1

---
+++ osnove_Java/src/main/java/p13_05_2022/Git_Zadatak1.java
@@ -0,0 +1,10 @@
+package p13_05_2022;
+
+public class Git_Zadatak1 {
+    public static void main(String[] args) {
+        System.out.println("Git Status");
+        System.out.println("Git Add");
+        System.out.println("Git Commit -m");
+        System.out.println("Git Push");
+    }
+}
